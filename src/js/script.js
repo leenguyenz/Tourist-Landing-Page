63a94eb5863bf5224eb7275c752790ace85e8c67
leenguyenz <leenguyenz@hotmail.com>
2018-06-14 08:13:53 +0700

    change environment

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,10 @@ $('.nav-fixed').on("click", function(){
     $(this).toggleClass('bar-transform');
     $('nav').toggleClass('nav-block');
 })
+
+$('.btn').mouseover(function(){
+    $('.btn:after').animate({
+        transform: scale(1.5),
+        opacity:1
+    },500)
+})
