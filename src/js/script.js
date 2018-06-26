4b232ad385a587da41c1ec510df57e60d9e02b91
Nguyen Hoang Anh <anhnh@goquo.com>
2018-06-26 17:35:42 +0700

    update header

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,30 @@
 $( document ).ready(function() {
-    $('.nav-fixed').on("click", function(){
-        $(this).toggleClass('bar-transform');
-        $('nav').toggleClass('nav-block');
-    })
+    // $('.nav-fixed').on("click", function(){
+    //     $(this).toggleClass('bar-transform');
+    //     $('nav').toggleClass('nav-block');
+    // })
 
-    $.keyframe.define({
-        name: 'button-effect',
-        from: {
-            'transform':'scale(1)'
-        },
-        to: {
-            'transform':'scale(1.5)'
-        }
-    });
+    // $.keyframe.define({
+    //     name: 'button-effect',
+    //     from: {
+    //         'transform':'scale(1)'
+    //     },
+    //     to: {
+    //         'transform':'scale(1.5)'
+    //     }
+    // });
 
-    $('.btn').mouseover(function(){
-        $(this).addClass('btn-effect');
-        // $('.btn-effect').playKeyframe({
-        //     name: 'button-effect',
-        //     duration: '700ms'
-        // })
-    });
-$('.btn').mouseout(function(){
-    $(this).removeClass('btn-effect');
-});
+    // $('.btn').mouseover(function(){
+    //     $(this).addClass('btn-effect');
+    //     // $('.btn-effect').playKeyframe({
+    //     //     name: 'button-effect',
+    //     //     duration: '700ms'
+    //     // })
+    // });
+    
+// $('.btn').mouseout(function(){
+//     $(this).removeClass('btn-effect');
+// });
 });
 
 
